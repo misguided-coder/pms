20f6bb215194f2f5a92d79a42ca26de2e84820de
Sangeetha Krishnamurthi <shubhakrish@hotmail.com>
2017-11-17 10:09:05 +0530

    employee class added

---
+++ src/main/java/com/example/Employee.java
@@ -0,0 +1,24 @@
+package com.example;
+
+public class Employee{
+
+	int id;
+	String name;
+	
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+
+	
+}
